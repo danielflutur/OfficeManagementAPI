5b9fcbb42f6c0611b9d2bc65fa49d2a80d410cb7
Daniel Ioan Flutur <ioan.flutur@student.usv.ro>
2022-03-11 23:23:02 +0200

    Am adaygat un nou tabel ce se ocupa de cererile de munca de la distanta si am implementat functionalitatea acestuia

---
+++ OfficeManagementAPI/Controllers/RequestsController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using OfficeManagementAPI.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OfficeManagementAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public RequestsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                            select RequestNo, EmployeeName, RemotePercent, RequestMsg from dbo.Requests
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CompanyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Requests req)
+        {
+            string query = @"
+                            insert into dbo.Requests values(
+                            @EmployeeName, @RemotePercent, @RequestMsg
+                            )
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CompanyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@EmployeeName", req.EmployeeName);
+                    myCommand.Parameters.AddWithValue("@RemotePercent", req.RemotePercent);
+                    myCommand.Parameters.AddWithValue("@RequestMsg", req.RequestMsg);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Added succesfuly");
+        }
+        [HttpPut]
+        public JsonResult Put(Requests req)
+        {
+            string query = @"
+                            update dbo.Requests
+                            set
+                            EmployeeName = @EmployeeName,
+                            RemotePercent = @RemotePercent,
+                            RequestMsg = @RequestMsg
+                            where RequestNo = @RequestNo
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CompanyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@EmployeeName", req.EmployeeName);
+                    myCommand.Parameters.AddWithValue("@RemotePercent", req.RemotePercent);
+                    myCommand.Parameters.AddWithValue("@RequestMsg", req.RequestMsg);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Updated succesfuly");
+        }
+        [HttpDelete("{name}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                            delete from dbo.Requests where RequestNo = @RequestNo
+                            ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("CompanyAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@RequestNo", id);
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Deleted succesfuly");
+        }
+    }
+}
---
+++ OfficeManagementAPI/Models/Requests.cs
@@ -0,0 +1,10 @@
+﻿namespace OfficeManagementAPI.Models
+{
+    public class Requests
+    {
+        public int RequestNo { get; set; }
+        public string EmployeeName { get; set; }
+        public int RemotePercent { get; set; }
+        public string RequestMsg { get; set; }
+    }
+}
